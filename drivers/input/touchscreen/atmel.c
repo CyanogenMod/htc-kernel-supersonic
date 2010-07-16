b674bf8c6e354c4790678be9723b882c3d6a07f2
netarchy <netarchy@gmail.com>
2010-07-16 17:28:58 +0800

    Integrated ffolkes atmel code to set 3point for sensors that dont support 5point multitouch

--- drivers/input/touchscreen/atmel.c
+++ drivers/input/touchscreen/atmel.c
@@ -902,6 +902,22 @@ static int atmel_ts_probe(struct i2c_client *client,
 		ts->id->matrix_x_size, ts->id->matrix_y_size,
 		ts->id->num_declared_objects);
 
+	/*
+	 Begin ffolkes multitouch selection
+	  Purpose: Some hardware can't support more than 3 multitouch points, so revert back to 3 if we find a 004F touch sensor
+	 BEGIN
+	*/
+	if(ts->id->family_id == 0x4F && pdata->config_T9[14] != 3) {
+		printk(KERN_INFO "%d point multitouch disabled due to possible hardware conflict, reverting to 3 point\n", pdata->config_T9[14]);
+		pdata->config_T9[14] = 3;
+	} else {
+		printk(KERN_INFO "%d point multitouch enabled\n", pdata->config_T9[14]);
+	}
+	/*
+	 END
+	*/
+		
+		
 	/* Read object table. */
 	ts->object_table = kzalloc(sizeof(struct object_t)*ts->id->num_declared_objects, GFP_KERNEL);
 	if (ts->object_table == NULL) {
