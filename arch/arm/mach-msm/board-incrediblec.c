bff49d91e52a6aeb5272ade40b49e9baa8b8ed43
Steve Kondik <shade@chemlab.org>
2010-08-11 22:50:38 -0400

    inc: Set min_uV to 950

--- arch/arm/mach-msm/board-incrediblec.c
+++ arch/arm/mach-msm/board-incrediblec.c
@@ -715,7 +715,7 @@ static struct regulator_init_data tps65023_data[5] = {
 	{
 		.constraints = {
 			.name = "dcdc1", /* VREG_MSMC2_1V29 */
-			.min_uV = 1000000,
+			.min_uV = 950000,
 			.max_uV = 1300000,
 			.valid_ops_mask = REGULATOR_CHANGE_VOLTAGE,
 		},
